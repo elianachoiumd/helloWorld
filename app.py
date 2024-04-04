acaaae4dc3ea0b56f5105e51440548e20045a2cb
Eliana Choi <eliechoi@terpmail.umd.edu>
2024-04-04 11:35:28 -0400

    Adding email to create route on app.py

--- app.py
+++ app.py
@@ -47,12 +47,13 @@ def student_create():
         first_name = request.form['first_name']
         last_name = request.form['last_name']
         major_id = request.form['major_id']
-
+        email = request.form['email']
         birth_date = request.form['birth_date']
         is_honors = True if 'is_honors' in request.form else False
 
         student = Student(first_name=first_name, last_name=last_name, major_id=major_id,
-                          birth_date=dt.strptime(birth_date, '%Y-%m-%d'), is_honors=is_honors)
+                          email=email.str(email, '\w@\.\w{3}'), birth_date=dt.strptime(birth_date, '%Y-%m-%d'),
+                          is_honors=is_honors)
         db.session.add(student)
         db.session.commit()
         flash(f'{first_name} {last_name} was successfully added!', 'success')
@@ -83,6 +84,7 @@ def student_edit(student_id):
             student.first_name = request.form['first_name']
             student.last_name = request.form['last_name']
             student.major_id = request.form['major_id']
+            student.email = request.form['email']
             student.birthdate = dt.strptime(request.form['birth_date'], '%Y-%m-%d')
             student.num_credits_completed = request.form['num_credits_completed']
             student.gpa = request.form['gpa']
